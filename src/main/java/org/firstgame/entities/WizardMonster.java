1409a0af365706888fd9bd4facc5ed5902a9100a
umutkutlu2 <91705026+umutkutlu2@users.noreply.github.com>
2024-12-25 18:30:56 +0300

    Update WizardMonster.java
    wizardMonster updated

--- src/main/java/org/firstgame/entities/WizardMonster.java
+++ src/main/java/org/firstgame/entities/WizardMonster.java
@@ -1,4 +1,35 @@
 package org.firstgame.entities;
 
-public class WizardMonster {
+import java.util.Random;
+
+import org.firstgame.properties.Constants;
+
+public class WizardMonster extends Monster {
+
+    // Constructor for WizardMonster
+    public WizardMonster() {
+        super(); // Call to the superclass (Monster) constructor
+
+        // Set the sprite image for the WizardMonster
+        setSprite(Constants.WIZARD_SPRITE);
+
+        // Generate a random position within a range (-150 to 150 for both x and y)
+        Random rand = new Random();
+        int x = rand.nextInt(300) - 150;
+        int y = rand.nextInt(300) - 150;
+
+        // Set the position of the WizardMonster
+        setPosition(x, y);
+    }
+
+    // Implementation of abstract method from Monster
+    @Override
+    public void performAction() {
+        // Unique behavior for the WizardMonster
+        System.out.println("WizardMonster is teleporting the rune!");
+        // Add teleportation logic here
+    }
 }
+
+
+
