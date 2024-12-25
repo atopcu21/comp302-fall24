91d4e248b503efda786c0d32f54dc2b466ae9960
umutkutlu2 <91705026+umutkutlu2@users.noreply.github.com>
2024-12-25 18:22:30 +0300

    Add files via upload
    constants

---
+++ src/main/java/org/firstgame/Constants.java
@@ -0,0 +1,21 @@
+package org.firstgame.properties;
+
+public class Constants {
+
+    // Key codes for arrow key inputs
+    public static final Integer KEY_LEFT_ARROW_CODE = 37; // Left arrow key
+    public static final Integer KEY_UP_ARROW_CODE = 38;   // Up arrow key
+    public static final Integer KEY_RIGHT_ARROW_CODE = 39; // Right arrow key
+    public static final Integer KEY_DOWN_ARROW_CODE = 40;  // Down arrow key
+
+    // File paths for sprite assets
+    public static final String PLAYER_SPRITE = "src/main/java/org/firstgame/assets/player.png";
+    public static final String FIGHTER_SPRITE = "src/main/java/org/firstgame/assets/fighter.png";
+    public static final String WIZARD_SPRITE = "src/main/java/org/firstgame/assets/wizard.png";
+    public static final String ARCHER_SPRITE = "src/main/java/org/firstgame/assets/archer.png";
+}
+
+
+
+
+
