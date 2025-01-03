5e81f71bf55f57d4c7e3ef88d22ab4db40f8f78e
umutkutlu2 <91705026+umutkutlu2@users.noreply.github.com>
2025-01-03 14:10:36 +0300

    Update ArcherMonster.java
    archerMonster updated (not final)

--- src/main/java/org/firstgame/entities/ArcherMonster.java
+++ src/main/java/org/firstgame/entities/ArcherMonster.java
@@ -1,35 +1,22 @@
-
 package org.firstgame.entities;
+
+import org.firstgame.RokueLikeGame;
+import org.firstgame.properties.Constants;
 
 import java.util.Random;
 
-import org.firstgame.properties.Constants;
-
 public class ArcherMonster extends Monster {
-
-    // Constructor for ArcherMonster
     public ArcherMonster() {
-        super(); // Call to the superclass (Monster) constructor
-
-        // Set the sprite image for the ArcherMonster
+        super();
         setSprite(Constants.ARCHER_SPRITE);
-
-        // Generate a random position within a range (-150 to 150 for both x and y)
         Random rand = new Random();
-        int x = rand.nextInt(300) - 150;
-        int y = rand.nextInt(300) - 150;
-
-        // Set the position of the ArcherMonster
+        int x = rand.nextInt(12);
+        int y = rand.nextInt(12);
         setPosition(x, y);
     }
 
-    // Implementation of the abstract performAction method from Monster
     @Override
-    public void performAction() {
-        // Define specific behavior for ArcherMonster
-        System.out.println("ArcherMonster is shooting arrows!");
-        // Add logic for shooting arrows or attacking the player here
+    public void onCollusion(GameObject otherObject) {
+        super.onCollusion(otherObject);
     }
 }
-
-
