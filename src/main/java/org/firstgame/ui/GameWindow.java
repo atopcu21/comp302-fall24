e3e49f823cb06da3f013ab624f0b80119aa464c5
umutkutlu2 <91705026+umutkutlu2@users.noreply.github.com>
2025-01-03 14:34:34 +0300

    Update GameWindow.java

--- src/main/java/org/firstgame/ui/GameWindow.java
+++ src/main/java/org/firstgame/ui/GameWindow.java
@@ -1,50 +1,53 @@
 package org.firstgame.ui;
 
 import org.firstgame.RokueLikeGame;
-import org.firstgame.properties.Rotation;
-import org.firstgame.properties.ScreenPosition;
-import org.firstgame.properties.WorldPosition;
-import org.firstgame.entities.GameObject;
+import org.firstgame.entities.*;
+import org.firstgame.properties.*;
 
 import javax.imageio.ImageIO;
 import javax.swing.*;
 import java.awt.*;
-import java.awt.event.ActionEvent;
-import java.awt.event.ActionListener;
 import java.awt.event.KeyEvent;
 import java.awt.event.KeyListener;
+import java.awt.event.MouseEvent;
+import java.awt.event.MouseListener;
 import java.awt.geom.AffineTransform;
 import java.awt.image.BufferedImage;
 import java.io.File;
 import java.io.IOException;
+import java.util.List;
+import java.util.Random;
 
-public class GameWindow extends JPanel implements KeyListener {
+public class GameWindow extends JPanel implements KeyListener, MouseListener {
     private static GameWindow instance;
     private RokueLikeGame gameInstance;
     private final Timer gameTimer;
     private JLabel timeLabel;
+    private JLabel runeLabel;
     private long startTime;
-    private boolean isPaused;
+    private boolean runeFound;
+    private boolean monstersGenerating;
 
     private GameWindow() {
-        setBackground(new Color(40, 10, 35));
-        JFrame frame = new JFrame("First Game");
+        JFrame frame = new JFrame("Rokue Like Game");
         frame.add(this);
+        setBackground(new Color(40, 10, 35));
         addKeyListener(this);
         this.setFocusable(true);
         this.requestFocusInWindow();
 
         this.setLayout(null);
         frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
-        frame.setSize(800, 600);
+        frame.setSize(1280, 720);
         frame.setLocationRelativeTo(null);
         frame.setResizable(false);
         frame.setVisible(true);
 
+        addMouseListener(this);
+
         gameTimer = new Timer(16, e -> updateScreen());
         gameTimer.start();
         startTime = System.currentTimeMillis();
-        isPaused = false;
 
         timeLabel = new JLabel("Time: 0 seconds");
         timeLabel.setBounds((getWidth() / 2) - 200, 10, 400, 30);
@@ -57,13 +60,21 @@ public class GameWindow extends JPanel implements KeyListener {
         } catch (FontFormatException | IOException ignored) {
 
         }
-        this.add(timeLabel);
 
-        createPauseResumeButtons();
+        runeLabel = new JLabel("");
+        runeLabel.setBounds((getWidth() / 2) - 200, 40, 400, 30);
+        runeLabel.setLayout(new FlowLayout());
+        runeLabel.setHorizontalAlignment(SwingConstants.CENTER);
+        runeLabel.setForeground(Color.WHITE);
+        try {
+            Font customFont = Font.createFont(Font.TRUETYPE_FONT, new File("src/main/java/org/firstgame/fonts/custom_font.ttf")).deriveFont(14f);
+            runeLabel.setFont(customFont);
+        } catch (FontFormatException | IOException ignored) {
 
-        if (!gameTimer.isRunning()) {
-            gameTimer.start();
         }
+
+        this.add(timeLabel);
+        this.add(runeLabel);
     }
 
     public void setGameInstance(RokueLikeGame gameInstance) {
@@ -71,7 +82,7 @@ public class GameWindow extends JPanel implements KeyListener {
     }
 
     public static GameWindow getInstance() {
-        if (instance == null) {
+        if(instance == null) {
             instance = new GameWindow();
         }
         return instance;
@@ -80,8 +91,8 @@ public class GameWindow extends JPanel implements KeyListener {
     @Override
     public void paintComponent(Graphics g) {
         super.paintComponent(g);
-        if (gameInstance != null && !isPaused) {
-            for (GameObject gameObject : gameInstance.getGameObjects()) {
+        if(gameInstance != null) {
+            for(GameObject gameObject : gameInstance.getGameObjects()) {
                 BufferedImage image = null;
                 try {
                     image = ImageIO.read(new File(gameObject.getSprite()));
@@ -92,83 +103,129 @@ public class GameWindow extends JPanel implements KeyListener {
 
                     AffineTransform transform = new AffineTransform();
 
-                    if (gameObject.getFacingDirection() == Rotation.LEFT) {
-                        transform.scale(-1, 1); // Mirror horizontally
-                        transform.translate(-width, 0); // Adjust to original position
+                    if(gameObject.getFacingDirection() == Rotation.LEFT){
+                        transform.scale(-1, 1);
+                        transform.translate(-width, 0);
                     } else {
-                        transform.scale(1, 1); // Normal rendering
+                        transform.scale(1, 1);
                     }
 
-                    // Apply the transform
                     g2.drawImage(image, transform, null);
 
-                    // Draw on screen
+                    if(gameObject.hasRune()){
+                        bi = increaseBrightness(bi, 50);
+                    }
+
                     g.drawImage(bi,
                             worldPositionToScreenPosition(gameObject.getPosition()).x() - image.getWidth(this) / 2,
                             worldPositionToScreenPosition(gameObject.getPosition()).y() - image.getHeight(this) / 2,
                             this);
                 } catch (IOException e) {
-                    System.err.println("Error loading sprite: " + gameObject.getSprite());
+                    // throw new RuntimeException(e);
                 }
             }
         }
     }
 
     public void updateScreen() {
-        if (!isPaused) {
-            RokueLikeGame.getInstance().movePlayer();
-            updateTime();
-            repaint();
+        RokueLikeGame.getInstance().movePlayer();
+        generateMonsters();
+        moveMonsters();
+        checkForCollisions();
+        checkRuneFound();
+        updateTime();
+        repaint();
+    }
+
+    public void checkForCollisions() {
+        for (GameObject gameObject : RokueLikeGame.getInstance().getGameObjects()) {
+            gameObject.checkForCollisions();
+        }
+    }
+
+    public void generateMonsters() {
+        if(((System.currentTimeMillis() - startTime) / 1000) % 8 == 7){
+            if(!monstersGenerating){
+                monstersGenerating = true;
+                Random r = new Random();
+                int i = r.nextInt(3);
+                GameObject monster = null;
+                switch (i) {
+                    case 0: {
+                        monster = new FighterMonster();
+                        break;
+                    }
+                    case 1: {
+                        monster = new ArcherMonster();
+                        break;
+                    }
+                    case 2: {
+                        monster = new WizardMonster();
+                        break;
+                    }
+                }
+                RokueLikeGame.getInstance().addGameObject(monster);
+            }
+        } else {
+            monstersGenerating = false;
+        }
+    }
+
+    public void moveMonsters() {
+        for (GameObject gameObject : RokueLikeGame.getInstance().getGameObjects()) {
+            if(gameObject instanceof FighterMonster){
+                if(((System.currentTimeMillis() - startTime) / 1000) % 3 == 0){
+                    Random r = new Random();
+                    ((FighterMonster) gameObject).setRotation(new Rotation(r.nextInt(360)));
+                } else {
+                    ((FighterMonster) gameObject).move();
+                }
+            } else if (gameObject instanceof WizardMonster) {
+                if(((System.currentTimeMillis() - startTime) / 1000) % 5 == 4){
+                    List<GameObject> runeCarryingObjects = RokueLikeGame.getInstance()
+                            .getGameObjects()
+                            .stream()
+                            .filter(it -> it.canHaveRuneInIt() && !it.hasRune()).toList();
+                    List<GameObject> theRune = RokueLikeGame.getInstance()
+                            .getGameObjects()
+                            .stream()
+                            .filter(it -> it.canHaveRuneInIt() && it.hasRune()).toList();
+                    if(!theRune.isEmpty()){
+                        theRune.get(0).setHasRune(false);
+                        Random r = new Random();
+                        int n = r.nextInt(runeCarryingObjects.size());
+                        runeCarryingObjects.get(n).setHasRune(true);
+                        gameObject.setSprite(Constants.WIZARD_CHARGED_SPRITE);
+                    }
+                } else if(((System.currentTimeMillis() - startTime) / 1000) % 5 == 0){
+                    gameObject.setSprite(Constants.WIZARD_SPRITE);
+                }
+            }
+        }
+    }
+
+    public void checkRuneFound() {
+        if(runeFound){
+            runeLabel.setText("Rune Found");
         }
     }
 
     public void updateTime() {
         long timeElapsed = (System.currentTimeMillis() - startTime) / 1000;
-        if (gameInstance.getAdventureTime() - timeElapsed <= 0) {
+        if(gameInstance.getAdventureTime() - timeElapsed <= 0){
             timeLabel.setText("Game Over");
             gameInstance.gameOver();
-        } else {
-            timeLabel.setText("Time: " + formatTimeRemaining(gameInstance.getAdventureTime() - timeElapsed));
-        }
+        } else timeLabel.setText(gameInstance.getCurrentLevel() + " Hall / Time: " + formatTimeRemaining(gameInstance.getAdventureTime() - timeElapsed));
     }
 
     public String formatTimeRemaining(long seconds) {
-        if (seconds < 60) {
+        if(seconds < 60) {
             return seconds + " seconds";
-        } else if (seconds < 3600) {
+        } else if(seconds < 3600) {
             return seconds / 60 + " minutes " + (seconds % 60) + " seconds";
-        } else if (seconds < 86400) {
+        } else if(seconds < 86400) {
             return seconds / 3600 + " hours " + ((seconds / 60) % 60) + " minutes " + (seconds % 60) + " seconds";
-        } else {
-            return "-";
-        }
-    }
-
-    private void createPauseResumeButtons() {
-        JButton pauseButton = new JButton("Pause");
-        pauseButton.setBounds(10, 10, 100, 30);
-        pauseButton.addActionListener(new ActionListener() {
-            @Override
-            public void actionPerformed(ActionEvent e) {
-                isPaused = true;
-                timeLabel.setText("Game Paused");
-                gameTimer.stop(); // Stop the timer
-            }
-        });
-
-        JButton resumeButton = new JButton("Resume");
-        resumeButton.setBounds(120, 10, 100, 30);
-        resumeButton.addActionListener(new ActionListener() {
-            @Override
-            public void actionPerformed(ActionEvent e) {
-                isPaused = false;
-                timeLabel.setText("Game Resumed");
-                gameTimer.start(); // Restart the timer
-            }
-        });
-
-        this.add(pauseButton);
-        this.add(resumeButton);
+        } else return "-";
     }
 
     @Override
@@ -177,19 +234,95 @@ public class GameWindow extends JPanel implements KeyListener {
 
     @Override
     public void keyPressed(KeyEvent e) {
-        if (!isPaused) {
-            RokueLikeGame.getInstance().keyPressTriggered(e.getKeyCode());
-        }
+        RokueLikeGame.getInstance().keyPressTriggered(e.getKeyCode());
     }
 
     @Override
     public void keyReleased(KeyEvent e) {
-        if (!isPaused) {
-            RokueLikeGame.getInstance().keyReleaseTriggered(e.getKeyCode());
-        }
+        RokueLikeGame.getInstance().keyReleaseTriggered(e.getKeyCode());
     }
 
     public ScreenPosition worldPositionToScreenPosition(WorldPosition worldPosition) {
-        return new ScreenPosition((int) worldPosition.getX() + getWidth() / 2, (int) worldPosition.getY() + getHeight() / 2);
+        return new ScreenPosition((int) ((worldPosition.getX() + Constants.WORLD_MARGIN_X) * 44), (int) ((worldPosition.getY() + Constants.WORLD_MARGIN_Y) * 44));
+    }
+
+    public void renderInventory(Graphics g) {
+
+    }
+
+    @Override
+    public void mouseClicked(MouseEvent e) {
+
+    }
+
+    @Override
+    public void mousePressed(MouseEvent e) {
+
+    }
+
+    @Override
+    public void mouseReleased(MouseEvent e) {
+        for (GameObject gameObject : RokueLikeGame.getInstance().getGameObjects()) {
+            if(Math.abs(RokueLikeGame.getInstance().getPlayer().getPosition().getX() - gameObject.getPosition().getX()) +
+                    Math.abs(RokueLikeGame.getInstance().getPlayer().getPosition().getY() - gameObject.getPosition().getY()) <= 1){
+                if(gameObject.hasRune()) {
+                    runeFound = true;
+                    gameObject.setHasRune(false);
+                    List<GameObject> door = RokueLikeGame.getInstance().getGameObjects().stream().filter(it -> it instanceof Door).toList();
+                    if(!door.isEmpty()){
+                        Door d = (Door) door.get(0);
+                        d.setOpened(true);
+                    }
+                }
+            }
+        }
+    }
+
+    @Override
+    public void mouseEntered(MouseEvent e) {
+
+    }
+
+    @Override
+    public void mouseExited(MouseEvent e) {
+
+    }
+
+    public void stopTimer() {
+        gameTimer.stop();
+    }
+
+    public void winGame() {
+        gameInstance.addAdventureTime();
+        runeFound = false;
+        runeLabel.setText("");
+        repaint();
+    }
+
+    public BufferedImage increaseBrightness(BufferedImage image, int brightnessIncrease) {
+        int width = image.getWidth();
+        int height = image.getHeight();
+
+        BufferedImage brightenedImage = new BufferedImage(width, height, image.getType());
+
+        for (int y = 0; y < height; y++) {
+            for (int x = 0; x < width; x++) {
+                int rgb = image.getRGB(x, y);
+
+                int alpha = (rgb >> 24) & 0xFF;
+                int red = (rgb >> 16) & 0xFF;
+                int green = (rgb >> 8) & 0xFF;
+                int blue = rgb & 0xFF;
+
+                red = Math.min(255, red + brightnessIncrease);
+                green = Math.min(255, green + brightnessIncrease);
+                blue = Math.min(255, blue + brightnessIncrease);
+
+                int newRgb = (alpha << 24) | (red << 16) | (green << 8) | blue;
+                brightenedImage.setRGB(x, y, newRgb);
+            }
+        }
+
+        return brightenedImage;
     }
 }
