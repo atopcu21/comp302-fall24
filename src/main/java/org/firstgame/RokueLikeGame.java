0e78667a991073eb7bbfe059603eed778782df7d
umutkutlu2 <91705026+umutkutlu2@users.noreply.github.com>
2025-01-03 14:22:25 +0300

    Update RokueLikeGame.java
    rogue like game updated

--- src/main/java/org/firstgame/RokueLikeGame.java
+++ src/main/java/org/firstgame/RokueLikeGame.java
@@ -1,7 +1,6 @@
 package org.firstgame;
 
-import org.firstgame.entities.GameObject;
-import org.firstgame.entities.Player;
+import org.firstgame.entities.*;
 import org.firstgame.properties.Rotation;
 import org.firstgame.ui.GameWindow;
 
@@ -15,17 +14,20 @@ import static org.firstgame.properties.Constants.*;
 
 public class RokueLikeGame {
     private static RokueLikeGame instance;
-    private List<GameObject> gameObjects = new ArrayList<>();
+    private List<GameObject> gameObjectsEarth = new ArrayList<>();
+    private List<GameObject> gameObjectsAir = new ArrayList<>();
+    private List<GameObject> gameObjectsFire = new ArrayList<>();
+    private List<GameObject> gameObjectsWater = new ArrayList<>();
     private GameWindow gameWindow;
     private Set<Integer> activeKeys = new HashSet<>();
     private Player player;
     private long adventureTime;
     private boolean isGameOver;
+    private String currentLevel = "Earth";
 
     private RokueLikeGame() {
-        createPlayer();
         createGameObjects();
-        initGame();
+        createPlayer();
     }
 
     public static RokueLikeGame getInstance() {
@@ -37,8 +39,12 @@ public class RokueLikeGame {
 
     public void createPlayer(){
         player = new Player();
-        player.setSprite("src/main/java/org/firstgame/assets/player2xright.png");
-        gameObjects.add(player);
+        switch (currentLevel) {
+            case "Earth" -> gameObjectsEarth.add(player);
+            case "Air" -> gameObjectsAir.add(player);
+            case "Fire" -> gameObjectsFire.add(player);
+            case "Water" -> gameObjectsWater.add(player);
+        }
     }
 
     public void createGameObjects(){
@@ -60,19 +66,96 @@ public class RokueLikeGame {
     }
 
     public void initGameTimer(){
-        adventureTime = 90;
+        adventureTime = gameObjectsEarth.size() * 5L;
     }
 
     public void initGameObjects(){
+        initWalls();
+    }
 
+    public void initWalls(){
+        switch (currentLevel) {
+            case "Earth" -> {
+                for(int i = 0; i < 12; i++){
+                    gameObjectsEarth.add(new Wall(Rotation.UP, i, 0));
+                    if(i == 7){
+                        gameObjectsEarth.add(new Door(i, 11));
+                    } else {
+                        gameObjectsEarth.add(new Wall(Rotation.DOWN, i, 11));
+                    }
+                    gameObjectsEarth.add(new Wall(Rotation.LEFT, -0.5, i));
+                    gameObjectsEarth.add(new Wall(Rotation.RIGHT, 11.4, i));
+                }
+            }
+            case "Air" -> {
+                for(int i = 0; i < 12; i++){
+                    gameObjectsAir.add(new Wall(Rotation.UP, i, 0));
+                    if(i == 7){
+                        gameObjectsAir.add(new Door(i, 11));
+                    } else {
+                        gameObjectsAir.add(new Wall(Rotation.DOWN, i, 11));
+                    }
+                    gameObjectsAir.add(new Wall(Rotation.LEFT, -0.5, i));
+                    gameObjectsAir.add(new Wall(Rotation.RIGHT, 11.4, i));
+                }
+            }
+            case "Fire" -> {
+                for(int i = 0; i < 12; i++){
+                    gameObjectsFire.add(new Wall(Rotation.UP, i, 0));
+                    if(i == 7){
+                        gameObjectsFire.add(new Door(i, 11));
+                    } else {
+                        gameObjectsFire.add(new Wall(Rotation.DOWN, i, 11));
+                    }
+                    gameObjectsFire.add(new Wall(Rotation.LEFT, -0.5, i));
+                    gameObjectsFire.add(new Wall(Rotation.RIGHT, 11.4, i));
+                }
+            }
+            case "Water" -> {
+                for(int i = 0; i < 12; i++){
+                    gameObjectsWater.add(new Wall(Rotation.UP, i, 0));
+                    if(i == 7){
+                        gameObjectsWater.add(new Door(i, 11));
+                    } else {
+                        gameObjectsWater.add(new Wall(Rotation.DOWN, i, 11));
+                    }
+                    gameObjectsWater.add(new Wall(Rotation.LEFT, -0.5, i));
+                    gameObjectsWater.add(new Wall(Rotation.RIGHT, 11.4, i));
+                }
+            }
+        }
     }
 
     public List<GameObject> getGameObjects() {
-        return gameObjects;
+        return switch (currentLevel) {
+            case "Earth" -> gameObjectsEarth;
+            case "Air" -> gameObjectsAir;
+            case "Fire" -> gameObjectsFire;
+            case "Water" -> gameObjectsWater;
+            default -> null;
+        };
     }
 
     public void setGameObjects(List<GameObject> gameObjects) {
-        this.gameObjects = gameObjects;
+
+    }
+
+    public void addGameObjects(List<GameObject> gameObject, String level) {
+        switch (level) {
+            case "Earth" -> this.gameObjectsEarth.addAll(gameObject);
+            case "Air" -> this.gameObjectsAir.addAll(gameObject);
+            case "Fire" -> this.gameObjectsFire.addAll(gameObject);
+            case "Water" -> this.gameObjectsWater.addAll(gameObject);
+        }
+    }
+
+    public void addGameObject(GameObject gameObject) {
+        switch (currentLevel) {
+            case "Earth" -> this.gameObjectsEarth.add(gameObject);
+            case "Air" -> this.gameObjectsAir.add(gameObject);
+            case "Fire" -> this.gameObjectsFire.add(gameObject);
+            case "Water" -> this.gameObjectsWater.add(gameObject);
+        }
     }
 
     public GameWindow getGameWindow() {
@@ -95,12 +178,26 @@ public class RokueLikeGame {
         return adventureTime;
     }
 
+    public void addAdventureTime(long adventureTime) {
+        this.adventureTime += adventureTime;
+    }
+
+    public void addAdventureTime() {
+        switch (currentLevel) {
+            case "Earth" -> addAdventureTime(gameObjectsEarth.size() * 5L);
+            case "Air" -> addAdventureTime(gameObjectsAir.size() * 5L);
+            case "Fire" -> addAdventureTime(gameObjectsFire.size() * 5L);
+            case "Water" -> addAdventureTime(gameObjectsWater.size() * 5L);
+        }
+    }
+
     public void gameOver() {
         isGameOver = true;
         activeKeys.clear();
         for (KeyListener keyListener : gameWindow.getKeyListeners()) {
             gameWindow.removeKeyListener(keyListener);
         }
+        gameWindow.stopTimer();
     }
 
     public void movePlayer() {
@@ -109,10 +206,10 @@ public class RokueLikeGame {
         } else if(activeKeys.size() == 1) {
             if (activeKeys.contains(KEY_LEFT_ARROW_CODE)) {
                 player.move(Rotation.LEFT);
-                player.setSprite("src/main/java/org/firstgame/assets/player2xleft.png");
+                player.setFacingDirection(Rotation.LEFT);
             } else if (activeKeys.contains(KEY_RIGHT_ARROW_CODE)) {
                 player.move(Rotation.RIGHT);
-                player.setSprite("src/main/java/org/firstgame/assets/player2xright.png");
+                player.setFacingDirection(Rotation.RIGHT);
             } else if (activeKeys.contains(KEY_UP_ARROW_CODE)) {
                 player.move(Rotation.UP);
             } else if (activeKeys.contains(KEY_DOWN_ARROW_CODE)) {
@@ -121,17 +218,40 @@ public class RokueLikeGame {
         } else if (activeKeys.size() == 2) {
             if (activeKeys.contains(KEY_LEFT_ARROW_CODE) && activeKeys.contains(KEY_UP_ARROW_CODE)) {
                 player.move(Rotation.UP_LEFT);
-                player.setSprite("src/main/java/org/firstgame/assets/player2xleft.png");
+                player.setFacingDirection(Rotation.LEFT);
             } else if (activeKeys.contains(KEY_RIGHT_ARROW_CODE) && activeKeys.contains(KEY_UP_ARROW_CODE)) {
                 player.move(Rotation.UP_RIGHT);
-                player.setSprite("src/main/java/org/firstgame/assets/player2xright.png");
+                player.setFacingDirection(Rotation.RIGHT);
             } else if (activeKeys.contains(KEY_RIGHT_ARROW_CODE) && activeKeys.contains(KEY_DOWN_ARROW_CODE)) {
                 player.move(Rotation.DOWN_RIGHT);
-                player.setSprite("src/main/java/org/firstgame/assets/player2xright.png");
+                player.setFacingDirection(Rotation.RIGHT);
             } else if (activeKeys.contains(KEY_LEFT_ARROW_CODE) && activeKeys.contains(KEY_DOWN_ARROW_CODE)) {
                 player.move(Rotation.DOWN_LEFT);
-                player.setSprite("src/main/java/org/firstgame/assets/player2xleft.png");
+                player.setFacingDirection(Rotation.LEFT);
             }
         }
+    }
+
+    public Player getPlayer() {
+        return player;
+    }
+
+    public void winGame() {
+        switch (currentLevel) {
+            case "Earth" -> currentLevel = "Air";
+            case "Air" -> currentLevel = "Water";
+            case "Water" -> currentLevel = "Fire";
+            default -> {
+                return;
+            }
+        }
+        gameWindow.winGame();
+        initWalls();
+        createGameObjects();
+        createPlayer();
+    }
+
+    public String getCurrentLevel() {
+        return currentLevel;
     }
 }
