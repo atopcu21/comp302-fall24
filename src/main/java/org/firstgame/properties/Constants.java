33e2f2b9b15fa30950ff26e092d4de32d47299c6
umutkutlu2 <91705026+umutkutlu2@users.noreply.github.com>
2025-01-03 14:32:17 +0300

    Update Constants.java

--- src/main/java/org/firstgame/properties/Constants.java
+++ src/main/java/org/firstgame/properties/Constants.java
@@ -5,4 +5,25 @@ public class Constants {
     public static final Integer KEY_UP_ARROW_CODE = 38;
     public static final Integer KEY_RIGHT_ARROW_CODE = 39;
     public static final Integer KEY_DOWN_ARROW_CODE = 40;
+
+    public static final String PLAYER_SPRITE = "src/main/java/org/firstgame/assets/player.png";
+    public static final String FIGHTER_SPRITE = "src/main/java/org/firstgame/assets/fighter.png";
+    public static final String WIZARD_SPRITE = "src/main/java/org/firstgame/assets/wizard.png";
+    public static final String WIZARD_CHARGED_SPRITE = "src/main/java/org/firstgame/assets/wizard_charged.png";
+    public static final String ARCHER_SPRITE = "src/main/java/org/firstgame/assets/archer.png";
+    public static final String WALL_RIGHT_SPRITE = "src/main/java/org/firstgame/assets/wall_right.png";
+    public static final String WALL_LEFT_SPRITE = "src/main/java/org/firstgame/assets/wall_left.png";
+    public static final String WALL_UP_SPRITE = "src/main/java/org/firstgame/assets/wall_up.png";
+    public static final String WALL_DOWN_SPRITE = "src/main/java/org/firstgame/assets/wall_down.png";
+    public static final String BUILD_CHEST_SPRITE = "src/main/java/org/firstgame/assets/build_chest.png";
+    public static final String BUILD_BOX_SMALL_SPRITE = "src/main/java/org/firstgame/assets/box_small.png";
+    public static final String BUILD_BOX_DOUBLE_SPRITE = "src/main/java/org/firstgame/assets/box_double.png";
+    public static final String BUILD_COLUMN_SPRITE = "src/main/java/org/firstgame/assets/column.png";
+    public static final String DOOR_CLOSED_SPRITE = "src/main/java/org/firstgame/assets/door_closed.png";
+    public static final String DOOR_OPENED_SPRITE = "src/main/java/org/firstgame/assets/door_open.png";
+    public static final String NEXT_BUTTON_SPRITE = "src/main/java/org/firstgame/assets/next.png";
+    public static final String PLAY_BUTTON_SPRITE = "src/main/java/org/firstgame/assets/play.png";
+
+    public static final double WORLD_MARGIN_X = 4;
+    public static final double WORLD_MARGIN_Y = 2;
 }
