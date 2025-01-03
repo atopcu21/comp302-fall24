e79eddf4c52c94e5b9dfad08ab45d8938e2db407
umutkutlu2 <91705026+umutkutlu2@users.noreply.github.com>
2025-01-03 14:23:14 +0300

    Update Main.java

--- src/main/java/org/firstgame/Main.java
+++ src/main/java/org/firstgame/Main.java
@@ -1,11 +1,13 @@
 package org.firstgame;
 
+import org.firstgame.ui.MainMenu;
+
+import javax.swing.*;
 import java.util.ArrayList;
 import java.util.List;
 
 public class Main {
-    public static RokueLikeGame gameInstance;
     public static void main(String[] args) {
-        gameInstance = RokueLikeGame.getInstance();
+        SwingUtilities.invokeLater(MainMenu::new);
     }
 }
